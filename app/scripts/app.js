0138a048f7d19a640e7961e1c4c88302efc7d260
David Calhoun <dpcalhoun@gmail.com>
2014-08-14 22:55:50 -0500

    Migrate pages to HTML. Call attention to menu button.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,21 @@
 (function() {
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    var menuSealed = localStorage['menuSealed'];
+    menuSealed = (menuSealed) ? JSON.parse(menuSealed) : true;
+
+    if (menuSealed) {
+      var tadaMenu = setInterval(wiggle, 8000);
+
+      $('#js-nav-toggle').on('click', function() {
+        localStorage['menuSealed'] = false;
+        clearInterval(tadaMenu);
+      });
+    }
+  });
+
+  var wiggle = function() {
+    $('#js-nav-toggle').removeClass('fadeInDown tada').delay(300).queue(function() {
+      $(this).addClass('tada').dequeue();
+    });
+  }
 })(this);
